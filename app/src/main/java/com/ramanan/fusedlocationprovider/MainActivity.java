caad754a0a1c6be0f167a0757f4047aae00de814
RamananG <ramanangrs@users.noreply.github.com>
2018-05-03 17:18:56 +0530

    updated GPS Enable option
    LocationSettingsRequest to check the status of GPS

--- app/src/main/java/com/ramanan/fusedlocationprovider/MainActivity.java
+++ app/src/main/java/com/ramanan/fusedlocationprovider/MainActivity.java
@@ -5,12 +5,25 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.ramanan.fusedlocationprovider.services.LocationService;
 import com.ramanan.fusedlocationprovider.utils.Utility;
 
@@ -19,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     private Activity A_;
     private TextView currentLocation;
+    private static final int REQUEST_PERMISSIONS_LOCATION_SETTINGS_REQUEST_CODE = 33;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,16 +46,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        //GPS
-        if (Utility.isGPSEnabled(A_)) {
-            if (Utility.checkLocationPermission(A_)) {
-                Intent intent = new Intent(A_, LocationService.class);
-                startService(intent);
-                //Register BroadcastReceiver to receive event from our location service
-                mLocationReceiver = new LocationReceiver();
-                registerReceiver(mLocationReceiver, new IntentFilter(LocationService.MY_LOCATION));
-                Log.i(TAG, " GPS and Location Services are started");
-            }
+        //PERMISSION
+        if (Utility.checkLocationPermission(A_)) {
+            registerLocationService();
         }
     }
 
@@ -55,14 +62,49 @@ public class MainActivity extends AppCompatActivity {
         Log.i("onPause", " GPS and Location Services are un-registered");
     }
 
+
+    private void registerLocationService() {
+        //LOCATION SERVICE
+        //Clients can also use Context.bindService() to obtain a persistent connection to a service.
+        Intent intent = new Intent(A_, LocationService.class);
+        startService(intent);
+        //Register BroadcastReceiver to receive event from our location service
+        mLocationReceiver = new LocationReceiver();
+        registerReceiver(mLocationReceiver, new IntentFilter(LocationService.MY_LOCATION));
+        Log.i(TAG, " Registered location broadcast receiver");
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case Utility.MY_PERMISSIONS_REQUEST_LOCATION:
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission has been granted.
+                    registerLocationService();
+                } else {
+                    // permission denied
+                    Toast.makeText(this, "You have denied permission to access location", Toast.LENGTH_LONG).show();
+                    finish();
+                }
+        }
+    }
+
     private class LocationReceiver extends BroadcastReceiver {
 
         @Override
         public void onReceive(Context context, Intent intent) {
             Bundle extras = intent.getExtras();
             if (extras != null) {
-                Location location = intent.getParcelableExtra(LocationService.INTENT_LOCATION_VALUE);
-                handleLocationUpdates(location);
+                if (extras.containsKey(LocationService.GPS_NOT_ENABLED)) {
+                    LocationRequest locationRequest = intent.getParcelableExtra(LocationService.GPS_NOT_ENABLED);
+                    checkForLocationSettings(locationRequest);
+                }
+                if (extras.containsKey(LocationService.INTENT_LOCATION_VALUE)) {
+                    Location location = intent.getParcelableExtra(LocationService.INTENT_LOCATION_VALUE);
+                    handleLocationUpdates(location);
+                }
             }
         }
 
@@ -74,5 +116,73 @@ public class MainActivity extends AppCompatActivity {
                 Double.toString(location.getLongitude());
         Log.d("LocationUpdates:", msg);
         currentLocation.setText(msg);
+    }
+
+
+    //Check for location settings.
+    public void checkForLocationSettings(LocationRequest locationRequest) {
+        try {
+            LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
+            builder.addLocationRequest(locationRequest);
+            SettingsClient settingsClient = LocationServices.getSettingsClient(MainActivity.this);
+
+            settingsClient.checkLocationSettings(builder.build())
+                    .addOnSuccessListener(A_, new OnSuccessListener<LocationSettingsResponse>() {
+                        @Override
+                        public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                            //Setting is success...
+                            Toast.makeText(A_, "Enabled the Location successfully. Now you can start", Toast.LENGTH_SHORT).show();
+                            Log.i(TAG, "Enabled the Location successfully.");
+                            registerLocationService();
+                        }
+                    })
+                    .addOnFailureListener(A_, new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            int statusCode = ((ApiException) e).getStatusCode();
+                            switch (statusCode) {
+                                case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                                    Log.i(TAG, "Location settings are not satisfied. Show the location prompt");
+                                    try {
+                                        // Show the dialog by calling startResolutionForResult(), and check the
+                                        // result in onActivityResult().
+                                        ResolvableApiException rae = (ResolvableApiException) e;
+                                        rae.startResolutionForResult(A_, REQUEST_PERMISSIONS_LOCATION_SETTINGS_REQUEST_CODE);
+                                    } catch (Exception e1) {
+                                        Log.i(TAG, "Location not satisfied. Show the location enable dialog Error :", e1);
+                                    }
+                                    break;
+                                case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                                    Log.i(TAG, "Setting change is not available. Try in another device");
+                                    Toast.makeText(A_, "Setting change is not available.Try in another device.", Toast.LENGTH_LONG).show();
+                            }
+
+                        }
+                    });
+
+        } catch (Exception ex) {
+            Log.i(TAG, "checkForLocationSettings :", ex);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            // Check for the integer request code originally supplied to startResolutionForResult().
+            case REQUEST_PERMISSIONS_LOCATION_SETTINGS_REQUEST_CODE:
+                switch (resultCode) {
+                    case Activity.RESULT_OK:
+                        Log.i(TAG, "User agreed to make required location settings changes");
+                        //PERMISSION
+                        if (Utility.checkLocationPermission(A_)) {
+                            registerLocationService();
+                        }
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Log.i(TAG, "User chose not to make required location settings changes");
+                        break;
+                }
+                break;
+        }
     }
 }
